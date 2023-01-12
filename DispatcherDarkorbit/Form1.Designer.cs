ff99e30c0612cde78183315a0e4eb7fa91de4717
martin šindler <sindler.martin1@email.cz>
2023-01-12 17:56:58 +0100

    Add project files.

--- DispatcherDarkorbit/Form1.Designer.cs
+++ DispatcherDarkorbit/Form1.Designer.cs
@@ -32,29 +32,52 @@
             this.ServerTxt = new System.Windows.Forms.TextBox();
             this.LoginBtn = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SidTxt
             // 
-            this.SidTxt.Location = new System.Drawing.Point(12, 202);
+            this.SidTxt.Location = new System.Drawing.Point(12, 12);
             this.SidTxt.Name = "SidTxt";
             this.SidTxt.PlaceholderText = "SID";
-            this.SidTxt.Size = new System.Drawing.Size(279, 23);
+            this.SidTxt.Size = new System.Drawing.Size(151, 23);
             this.SidTxt.TabIndex = 0;
             // 
             // ServerTxt
             // 
-            this.ServerTxt.Location = new System.Drawing.Point(297, 202);
+            this.ServerTxt.Location = new System.Drawing.Point(169, 12);
             this.ServerTxt.Name = "ServerTxt";
             this.ServerTxt.PlaceholderText = "Server";
-            this.ServerTxt.Size = new System.Drawing.Size(75, 23);
+            this.ServerTxt.Size = new System.Drawing.Size(52, 23);
             this.ServerTxt.TabIndex = 1;
             // 
             // LoginBtn
             // 
-            this.LoginBtn.Location = new System.Drawing.Point(378, 202);
+            this.LoginBtn.Location = new System.Drawing.Point(227, 11);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(64, 24);
+            this.LoginBtn.Size = new System.Drawing.Size(49, 24);
             this.LoginBtn.TabIndex = 2;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = true;
@@ -62,23 +85,259 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(-1, 0);
+            this.richTextBox1.Location = new System.Drawing.Point(17, 184);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(464, 196);
+            this.richTextBox1.Size = new System.Drawing.Size(259, 99);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Slot 1";
+            this.label2.Click += new System.EventHandler(this.button_click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(59, 62);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 15);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "-//-";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(141, 138);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(52, 15);
+            this.label16.TabIndex = 21;
+            this.label16.Text = "s5 status";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(141, 119);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(52, 15);
+            this.label17.TabIndex = 20;
+            this.label17.Text = "s4 status";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(141, 101);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(52, 15);
+            this.label18.TabIndex = 19;
+            this.label18.Text = "s3 status";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(141, 82);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(52, 15);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "s2 status";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(141, 62);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(53, 15);
+            this.label20.TabIndex = 17;
+            this.label20.Text = "s1 Status";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(141, 38);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(38, 15);
+            this.label22.TabIndex = 28;
+            this.label22.Text = "status";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(59, 38);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(56, 15);
+            this.label23.TabIndex = 27;
+            this.label23.Text = "Time Left";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 28);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Send All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(74, 297);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(147, 50);
+            this.button8.TabIndex = 35;
+            this.button8.Text = "Start AutoDispatch";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 30);
+            this.button4.TabIndex = 36;
+            this.button4.Text = "Collect All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Slot 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Slot 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Slot 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 15);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Slot 5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "-//-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 15);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "-//-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(59, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 15);
+            this.label8.TabIndex = 43;
+            this.label8.Text = "-//-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(59, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 15);
+            this.label9.TabIndex = 44;
+            this.label9.Text = "-//-";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 28);
+            this.button2.TabIndex = 45;
+            this.button2.Text = "Refresh Stats";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(17, 166);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 15);
+            this.label11.TabIndex = 46;
+            this.label11.Text = "log:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 433);
+            this.ClientSize = new System.Drawing.Size(291, 359);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.LoginBtn);
             this.Controls.Add(this.ServerTxt);
             this.Controls.Add(this.SidTxt);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.button_click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,5 +349,28 @@
         private TextBox ServerTxt;
         private Button LoginBtn;
         private RichTextBox richTextBox1;
+        private Button button1;
+        private Label label2;
+        private Label label10;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label22;
+        private Label label23;
+        private Button button3;
+        private Button button8;
+        private Button button4;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label1;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Button button2;
+        private Label label11;
     }
 }
--- DispatcherDarkorbit/Form1.cs
+++ DispatcherDarkorbit/Form1.cs
@@ -1,17 +1,23 @@
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
+using System.Timers;
 using Dispatcher;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace DispatcherDarkorbit
 {
     public partial class Form1 : Form
     {
-        public static HttpManager Client { get; set; }
+        public static HttpManager Client { get; set; } // Client
+        string[] dispatchS = new string[5];
+        bool isRunning = false;
         public Form1()
         {
             InitializeComponent();
 
-            var cookie = new Cookie("dosid" , "");
+            var cookie = new Cookie("dosid", "");
+
         }
 
         private async void LoginBtn_Click(object sender, EventArgs e)
@@ -23,28 +29,160 @@ namespace DispatcherDarkorbit
             }
 
             var cookie = new Cookie("dosid", SidTxt.Text, "/", $"{ServerTxt.Text}.darkorbit.com");
-           
+
             Client = new();
             Client._cookies.Add(cookie);
             var result = await Client.GetAsyncLimit($"https://{ServerTxt.Text}.darkorbit.com/indexInternal.es?action=internalDispatch");
-            Console.WriteLine(result);
+            richTextBox1.Text = $"{DateTime.Now}" + " Log: " + "Loged in" + Environment.NewLine + richTextBox1.Text;
 
-            string endsAtSlot1 = "";
-            var time = Regex.Match(result, "dispatch_collect_elem_1\", \"(.*?)\"");
-            if (time.Success)
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (sender is Button)
             {
-                endsAtSlot1 = time.Groups[1].Value;
-                richTextBox1.Text += $"{DateTime.Now}" +  " Log: " + "Slot1 expected end: " + endsAtSlot1 + Environment.NewLine;
+                Console.WriteLine("Button: " + (string)(sender as Button).Text);
+                switch ((sender as Button).Text)
+                {
+                    case "Send All":
+                        SendAllDispatchers();
+                        break;
+                    case "Collect All":
+                        CollectAll();
+                        break;
+                    case "Refresh Stats":
+                        RefreshStats();
+                        break;
+                    case "Start AutoDispatch":
+                        isRunning = !isRunning;
+                        AutoDispatch();
+                        break;
+                }
+            }
+        }
+        public async Task SendAllDispatchers()
+        {
+            Console.WriteLine("Send Started");
+            for (int n = 1; n < 6; n++)
+            {
+                if (dispatchS[n - 1] == "Not Hired")
+                {
+                    var request = await Client.PostAsyncLimitRaw($"https://{ServerTxt.Text}.darkorbit.com/ajax/dispatch.php", "command=sendDispatch&dispatchId=dispatch_retriever_r01");
+                }
+            }
+            richTextBox1.Text = $"{DateTime.Now}" + " Log: Sent" + Environment.NewLine + richTextBox1.Text;
+            Console.WriteLine("Send Ended");
+        }
+        public async Task CollectAll()
+        {
+            Console.WriteLine("Collect Started");
+            for (int n = 1; n < 6; n++)
+            {
+                if (dispatchS[n - 1] == "Finished")
+                {
+                    var request = await Client.PostAsyncLimitRaw($"https://{ServerTxt.Text}.darkorbit.com/ajax/dispatch.php", $"command=collectDispatch&slot={n}");
+                }
+            }
+            richTextBox1.Text = $"{DateTime.Now}" + " Log: Collected" + Environment.NewLine + richTextBox1.Text;
+            Console.WriteLine("Collect Ended");
+
+        }
+        public async Task RefreshStats()
+        {
+
+            Console.WriteLine("Refresh Started");
+            var result = await Client.GetAsyncLimit($"https://{ServerTxt.Text}.darkorbit.com/indexInternal.es?action=internalDispatch");
+            double time = TimeSpan.Parse(DateTime.Now.ToString("HH:mm:ss")).TotalSeconds;
+            double[] dispatchT = new double[5];
+            string txt;
+            for (int n = 1; n < 6; n++)
+            {
+                var getTime = Regex.Match(result, $"dispatch_collect_elem_{n}\", \"(.*?)\"");
+                if (getTime.Success)
+                {
+
+                    dispatchT[n - 1] = TimeSpan.Parse(getTime.Groups[1].Value.Substring(11)).TotalSeconds - time;
+
+                    if (dispatchT[n - 1] <= 0)
+                    {
+                        dispatchT[n - 1] = 0;
+                        dispatchS[n - 1] = "Finished";
+                        txt = "-//-";
+                    }
+                    else
+                    {
+                        dispatchS[n - 1] = "Running";
+                        txt = secToDate(dispatchT[n - 1]);
+                    }
+
+                }
+                else
+                {
+                    dispatchT[n - 1] = 0;
+                    dispatchS[n - 1] = "Not Hired";
+                    txt = "-//-";
+
+                }
+                switch (n)
+                {
+                    case 1:
+                        label10.Text = txt;
+                        label20.Text = dispatchS[0];
+                        break;
+                    case 2:
+                        label1.Text = txt;
+                        label19.Text = dispatchS[1];
+                        break;
+                    case 3:
+                        label7.Text = txt;
+                        label18.Text = dispatchS[2];
+                        break;
+                    case 4:
+                        label8.Text = txt;
+                        label17.Text = dispatchS[3];
+                        break;
+                    case 5:
+                        label9.Text = txt;
+                        label16.Text = dispatchS[4];
+                        break;
+                }
+
             }
 
-            Console.WriteLine(endsAtSlot1);
+            richTextBox1.Text = $"{DateTime.Now}" + " Log: Status refreshed" + Environment.NewLine + richTextBox1.Text;
+            Console.WriteLine("Refresh Ended");
 
-            //var request = await Client.PostAsyncLimitRaw($"https://{ServerTxt.Text}.darkorbit.com/ajax/dispatch.php", "command=sendDispatch&dispatchId=dispatch_retriever_a01");
-            //Console.WriteLine("Success: " + request);
+        }
+        public string secToDate(double s)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(s);
+            return string.Format("{0:D2}:{1:D2}:{2:D2}",
+                t.Hours,
+                t.Minutes,
+                t.Seconds,
+                t.Milliseconds);
+        }
+        public async void AutoDispatch()
+        {
             
+            while (isRunning == true)
+            {
+                await RefreshStats();
+                if (!dispatchS.Contains("Running"))
+                {
+                    await CollectAll();
+                    await RefreshStats();
+                    await SendAllDispatchers();
+                    await RefreshStats();
+                    richTextBox1.Text = $"{DateTime.Now}" + " Log: Sleeping for 5 min" + Environment.NewLine + richTextBox1.Text;
+                    Console.WriteLine("Sended refreshed");
+                }
+                else
+                    Console.WriteLine("Nothing to do");
 
-            //commands command=collectDispatch&slot=2 // collect
-            //send //command=sendDispatch&dispatchId=dispatch_retriever_a01
+                Console.WriteLine("Sleeping for 5 min");
+                await Task.Delay(5*60*1000);
+            }
 
         }
     }
